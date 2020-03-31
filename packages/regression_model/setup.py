4039de346e7ae13c3f9039ade992bc6546a66371
manish <manishsr2204@gmail.com>
2020-03-31 11:51:09 +0530

    API Schema validation

--- packages/regression_model/setup.py
+++ packages/regression_model/setup.py
@@ -10,14 +10,14 @@ from setuptools import find_packages, setup
 
 # Package meta-data.
 NAME = 'regression_model'
-DESCRIPTION = 'Learning  "Deployment of Machine Learning Models".'
-URL = 'https://github.com/manishsr2204/deploying-machine-learning-models.git'
-EMAIL = 'manishsr2204@gmail.com'
-AUTHOR = 'Manish Singh Rautela'
+DESCRIPTION = 'Train and deploy regression model.'
+URL = 'your github project'
+EMAIL = 'your_email@email.com'
+AUTHOR = 'Your name'
 REQUIRES_PYTHON = '>=3.6.0'
 
 
-# Packages that are required for this module to be executed
+# What packages are required for this module to be executed?
 def list_reqs(fname='requirements.txt'):
     with open(fname) as fd:
         return fd.read().splitlines()
@@ -42,7 +42,7 @@ except FileNotFoundError:
 
 # Load the package's __version__.py module as a dictionary.
 ROOT_DIR = Path(__file__).resolve().parent
-PACKAGE_DIR = ROOT_DIR / 'regression_model'
+PACKAGE_DIR = ROOT_DIR / NAME
 about = {}
 with open(PACKAGE_DIR / 'VERSION') as f:
     _version = f.read().strip()
@@ -65,7 +65,7 @@ setup(
     install_requires=list_reqs(),
     extras_require={},
     include_package_data=True,
-    license='BSD 3',
+    license='MIT',
     classifiers=[
         # Trove classifiers
         # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
@@ -73,8 +73,6 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy'
     ],
